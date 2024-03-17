637cf2980132c4adf14f5a55dcaaa77c00ab7425
hk4011 <khalidhager07@gmail.com>
2024-03-17 15:14:07 +0200

    add unit test

---
+++ proj2.tests/AccountControllerTests.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Moq;
+using proj2.Controllers;
+using proj2.DTO;
+using proj2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proj2.tests
+{
+    public class AccountControllerTests
+    {
+        [Fact]
+        public async Task Registration_ValidModel_ReturnsOk()
+        {
+            // Arrange
+            var userStoreMock = new Mock<IUserStore<ApplicationUser>>();
+            var identityOptionsMock = new Mock<IOptions<IdentityOptions>>();
+
+            var userManagerMock = new Mock<UserManager<ApplicationUser>>(
+                userStoreMock.Object,
+                identityOptionsMock.Object,
+                null, null, null, null, null, null, null);
+
+            var configMock = new Mock<IConfiguration>();
+            var controller = new AccountController(userManagerMock.Object, configMock.Object);
+            var registerDto = new RegisterDTO
+            {
+                Email = "test@example.com",
+                UserName = "testuser",
+                Password = "TestPassword123@"
+            };
+
+            userManagerMock.Setup(m => m.CreateAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+
+            // Act
+            var result = await controller.Registertion(registerDto);
+
+            // Assert
+            var okResult = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal("Account successfully added", okResult.Value);
+            userManagerMock.VerifyAll();
+
+        }
+    }
+}
---
+++ proj2.tests/HolidaysControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using proj2.Controllers;
+using proj2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace proj2.tests
+{
+    public class HolidaysControllerTests
+    {
+        private DbContextOptions<HRContext> _options;
+        private HRContext _context;
+
+        public HolidaysControllerTests()
+        {
+            _options = new DbContextOptionsBuilder<HRContext>()
+                .UseInMemoryDatabase(databaseName: "Hrproj3")
+                .Options;
+            _context = new HRContext(_options);
+            SeedTestData();
+        }
+
+        private void SeedTestData()
+        {
+            var holidays = new List<Holiday>
+            {
+                new() { Id = 1, Name = "New Year", Date = new DateOnly(2024, 1, 1) },
+                new Holiday { Id = 2, Name = "Christmas", Date = new DateOnly(2024, 12, 25) },
+            };
+
+            _context.AddRange(holidays);
+            _context.SaveChanges();
+        }
+
+        [Fact]
+        public async Task GetHolidays_ReturnsAllHolidays()
+        {
+            // Arrange
+            var controller = new HolidaysController(_context);
+
+            // Act
+            var result = await controller.GetHolidays();
+
+            // Assert
+            var holidays = Assert.IsType<ActionResult<IEnumerable<Holiday>>>(result);
+            Assert.Equal(2, holidays.Value.Count());
+        }
+
+    }
+}
